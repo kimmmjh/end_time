416c205a67ccdcb4ad5d751566eed6ff0ebe8afc
kimmmjh <ds08152@gmail.com>
2026-04-04 02:48:03 +0900

    updated_3d

--- models/auxiliary_components/_circular_convolution_3d.py
+++ models/auxiliary_components/_circular_convolution_3d.py
@@ -1,14 +1,25 @@
 from torch import nn
 
 
-def circular_conv_3d(in_channels: int, out_channels: int, kernel_size: int, bias=False) -> nn.Conv3d:
-    """
-    Return a circular padded 3d convolution layer.
+import torch
+import torch.nn.functional as F
 
-    :param in_channels: The number of channels in the input.
-    :param out_channels: The number of channels in the output.
-    :param kernel_size: The kernel size.
-    :param bias: Whether bias is used.
-    :returns: The convolution layer.
+class SemiCircularConv3d(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int, kernel_size: int, bias=False):
+        super().__init__()
+        self.conv = nn.Conv3d(in_channels, out_channels, kernel_size, bias=bias, padding=0)
+        self.pad_size = kernel_size // 2
+
+    def forward(self, x):
+        # x is (B, C, D, H, W) -> (batch, channels, time, x, y)
+        # Pad W (Y) and H (X) circularly for Toric Code boundaries
+        x = F.pad(x, (self.pad_size, self.pad_size, self.pad_size, self.pad_size, 0, 0), mode='circular')
+        # Pad D (Time) with zeros, because Time does not loop!
+        x = F.pad(x, (0, 0, 0, 0, self.pad_size, self.pad_size), mode='constant', value=0)
+        return self.conv(x)
+
+def circular_conv_3d(in_channels: int, out_channels: int, kernel_size: int, bias=False) -> nn.Module:
     """
-    return nn.Conv3d(in_channels, out_channels, kernel_size, padding="same", padding_mode="circular", bias=bias)
+    Return a semi-circular padded 3d convolution layer.
+    """
+    return SemiCircularConv3d(in_channels, out_channels, kernel_size, bias=bias)
